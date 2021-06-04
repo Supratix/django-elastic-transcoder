3039ba94189b4d0564ffdb263c09f306379e3fde
Tobias Goecke <tobias.goecke@supratix.com>
2021-06-04 13:45:26 +0200

    adding django 3 support
    Signed-off-by: Tobias Goecke <tobias.goecke@supratix.com>

--- dj_elastictranscoder/models.py
+++ dj_elastictranscoder/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class EncodeJob(models.Model):
     STATE_CHOICES = (
         (0, 'Submitted'),
